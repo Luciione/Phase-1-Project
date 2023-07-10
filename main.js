7def4c8d530fc9631a37e89807ea3d3f2ae46350
+ <lucy.njoroge1@student.moringaschool.com>
2023-07-10 06:24:50 +0300

    Project Phase 1

--- main.js
+++ main.js
@@ -17,7 +17,7 @@ recipeCloseBtn.addEventListener('click', () => {
 function searchMeals() {
   const searchInput = document.getElementById('search-input').value.trim();
   if (searchInput !== '') {
-    fetch(`https://api.edamam.com/api/recipes/v2?type=public&q=${searchInput}&app_id=${appId}&app_key=${appKey}`)
+    fetch(`www.themealdb.com/api/json/v1/1/search.php?s=Arrabiata=${searchInput}&app_id=${appId}&app_key=${appKey}`)
       .then(response => response.json())
       .then(data => {
         if (data.count > 0) {
