cc2ad6114ca0f572b460cc6d6de9f860a755133c
+ <lucy.njoroge1@student.moringaschool.com>
2023-07-10 12:20:39 +0300

    Phase 1 project

--- main.js
+++ main.js
@@ -1,139 +1,75 @@
-const express = require('express');
-const app = express();
-
 const searchBtn = document.getElementById('search-btn');
 const mealList = document.getElementById('meal');
 const mealDetailsContent = document.querySelector('.meal-details-content');
 const recipeCloseBtn = document.getElementById('recipe-close-btn');
+const notFound = document.getElementById('notFound');
 
-const appId = '8cf7fb00';
-const appKey = 'e2558cd13d861421e5aa229f6f47551b';
-
-searchBtn.addEventListener('click', searchMeals);
+// event listeners
+searchBtn.addEventListener('click', getMealList);
 mealList.addEventListener('click', getMealRecipe);
 recipeCloseBtn.addEventListener('click', () => {
   mealDetailsContent.parentElement.classList.remove('showRecipe');
 });
 
-function searchMeals() {
-  const searchInput = document.getElementById('search-input').value.trim();
-  if (searchInput !== '') {
-    fetch(`https://api.example.com/recipes?search=${searchInput}&app_id=${appId}&app_key=${appKey}`)
-      .then(response => response.json())
-      .then(data => {
-        if (data.count > 0) {
-          const meals = data.hits.map(hit => hit.recipe);
-          displayMealList(meals);
-        } else {
-          mealList.innerHTML = 'No meals found.';
-        }
-      })
-      .catch(error => {
-        console.log(error);
-        mealList.innerHTML = 'An error occurred while fetching the data.';
-      });
-  } else {
-    mealList.innerHTML = 'Please enter an ingredient to search.';
-  }
+// get meal list that matches with the ingredients
+function getMealList() {
+  let searchInputTxt = document.getElementById('search-input').value.trim();
+  fetch(`https://www.themealdb.com/api/json/v1/1/filter.php?i=${searchInputTxt}`)
+    .then(response => response.json())
+    .then(data => {
+      let html = "";
+      if (data.meals) {
+        data.meals.forEach(meal => {
+          html += `
+            <div class="meal-item" data-id="${meal.idMeal}">
+              <div class="meal-img">
+                <img src="${meal.strMealThumb}" alt="food">
+              </div>
+              <div class="meal-name">
+                <h3>${meal.strMeal}</h3>
+                <a href="#" class="recipe-btn">Get Recipe</a>
+              </div>
+            </div>
+          `;
+        });
+        notFound.style.display = 'none';
+      } else {
+        html = "Sorry, we didn't find any meal!";
+        notFound.style.display = 'block';
+      }
+
+      mealList.innerHTML = html;
+    });
 }
 
-function displayMealList(meals) {
-  let html = '';
-  meals.forEach(meal => {
-    html += `
-      <div class="meal-item">
-        <div class="meal-img">
-          <img src="${meal.image}" alt="food" onclick="showRecipe(this)" data-instructions="${meal.instructions}" data-video="${meal.video}">
-        </div>
-        <div class="meal-name">
-          <h3>${meal.label}</h3>
-          <a href="#" class="recipe-btn" onclick="showRecipe(this)">Get Recipe</a>
-        </div>
-      </div>
-    `;
-  });
-  mealList.innerHTML = html;
-}
-
+// get recipe of the meal
 function getMealRecipe(e) {
   e.preventDefault();
   if (e.target.classList.contains('recipe-btn')) {
     let mealItem = e.target.parentElement.parentElement;
-    const mealId = mealItem.dataset.id;
-    fetch(`https://api.edamam.com/api/recipes/v2/${mealId}?type=public&app_id=${appId}&app_key=${appKey}`)
+    fetch(`https://www.themealdb.com/api/json/v1/1/lookup.php?i=${mealItem.dataset.id}`)
       .then(response => response.json())
-      .then(data => mealRecipeModal(data.recipe));
+      .then(data => mealRecipeModal(data.meals));
   }
 }
 
-function showRecipe(element) {
-  const recipeDetails = document.getElementById('meal-details');
-  const recipeImg = document.getElementById('recipe-img');
-  const recipeTitle = document.querySelector('.recipe-title');
-  const recipeCategory = document.querySelector('.recipe-category');
-  const recipeInstructions = document.querySelector('.recipe-instruct');
-  const recipeVideoLink = document.querySelector('.recipe-link a');
-  const mealName = element.parentNode.querySelector('h3').innerText;
-  const mealImgSrc = element.parentNode.querySelector('img').src;
-
-  recipeDetails.style.display = 'block';
-  recipeImg.src = mealImgSrc;
-  recipeTitle.innerText = mealName;
-  recipeInstructions.innerHTML = element.dataset.instructions;
-  recipeVideoLink.href = element.dataset.video;
-  recipeCategory.innerText = 'Category: Seafood';
-
-  const mealInstructions = element.parentNode.querySelector('.meal-instructions');
-  if (mealInstructions.innerHTML.trim() === '') {
-    const instructions = element.dataset.instructions;
-    mealInstructions.innerHTML = `<h3>Instructions:</h3><p>${instructions}</p>`;
-  }
-
-  const recipeCloseBtn = document.getElementById('recipe-close-btn');
-  recipeCloseBtn.addEventListener('click', () => {
-    hideRecipe();
-  });
-}
-
-function hideRecipe() {
-  const recipeDetails = document.getElementById('meal-details');
-  recipeDetails.style.display = 'none';
-}
-
-function getMealInstructions(element) {
-  const mealId = element.dataset.mealId;
-  fetch(`https://api.example.com/meals/${mealId}/instructions`)
-    .then(response => response.json())
-    .then(data => {
-      const instructions = data.instructions;
-      const mealInstructions = element.parentNode.querySelector('.meal-instructions');
-      if (mealInstructions.innerHTML.trim() === '') {
-        mealInstructions.innerHTML = `<h3>Instructions:</h3><p>${instructions}</p>`;
-      }
-    })
-    .catch(error => {
-      console.log(error);
-    });
-}
-
-function mealRecipeModal(recipe) {
-  const recipeTitle = recipe.label;
-  const recipeImage = recipe.image;
-  const recipeInstructions = recipe.instructions;
-  const recipeVideo = recipe.video;
-
-  const recipeTitleElement = document.querySelector('.recipe-title');
-  recipeTitleElement.innerText = recipeTitle;
-
-  const recipeImg = document.getElementById('recipe-img');
-  recipeImg.src = recipeImage;
-
-  const recipeInstructionsElement = document.querySelector('.recipe-instruct');
-  recipeInstructionsElement.innerHTML = `<h3>Instructions:</h3><p>${recipeInstructions}</p>`;
-
-  const recipeVideoLink = document.querySelector('.recipe-link a');
-  recipeVideoLink.href = recipeVideo;
-
-  const recipeDetails = document.getElementById('meal-details');
-  recipeDetails.style.display = 'block';
+// create a modal
+function mealRecipeModal(meal) {
+  meal = meal[0];
+  let html = `
+    <h2 class="recipe-title">${meal.strMeal}</h2>
+    <p class="recipe-category">${meal.strCategory}</p>
+    <div class="recipe-instruct">
+      <h3>Instructions:</h3>
+      <p>${meal.strInstructions}</p>
+    </div>
+    <div class="recipe-meal-img">
+      <img src="${meal.strMealThumb}" alt="">
+    </div>
+    <div class="recipe-link">
+      <a href="${meal.strYoutube}" target="_blank">Watch Video</a>
+    </div>
+  `;
+  mealDetailsContent.innerHTML = html;
+  mealDetailsContent.parentElement.classList.add('showRecipe');
 }
